df73f9ff998e1ed8070016652ddf9fda17b22072
Richard P. Asch <rpasch@rpclassics.net>
2014-04-07 10:28:31 -0700

    Initial creation; needed for unit tests

---
+++ PIMS.UnitTest/TestHelpers.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Hosting;
+using System.Web.Mvc;
+
+
+namespace PIMS.UnitTest
+{
+    public static class TestHelpers
+    {
+       
+        public static HttpRequestMessage GetHttpRequestMessage()
+        {
+            var request = new HttpRequestMessage();
+            request.Properties[HttpPropertyKeys.HttpConfigurationKey] = new HttpConfiguration();
+            return request;
+        }
+
+
+        public static HttpRequestMessage GetHttpRequestMessage(params object[] routeInfo) {
+
+            // Boiler plate setup: mimics controller context state setup that run-time engine expects
+            // when handling a POST; this may be unnecessary with WebApi v2.x?
+
+            /*   Indices:
+                 * 0 - HttpMethod VERB
+                 * 1 - target URL
+                 * 2 - controller (Api)
+                 * 3 - route name
+                 * 4 - route template
+                 * 5 - route default(s)
+            */
+
+
+            var request = new HttpRequestMessage( (HttpMethod) routeInfo[0], routeInfo[1].ToString());
+            var httpCfg = new HttpConfiguration();
+            request.Properties[HttpPropertyKeys.HttpConfigurationKey] = httpCfg;
+
+            /* Controller */
+            var ctrl = (ApiController) routeInfo[2];
+            ctrl.Request = request;
+            ctrl.Configuration = httpCfg;
+            
+            /* Route collection*/
+            httpCfg.Routes.MapHttpRoute(routeInfo[3].ToString(), routeInfo[4].ToString(), routeInfo[5]);
+
+            return request;
+        }
+    }
+}
