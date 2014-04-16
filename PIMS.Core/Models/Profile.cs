53373b37d5f29706eac0822b35e8ab74d1e18afa
Richard P. Asch <rpasch@rpclassics.net>
2014-04-15 17:00:54 -0700

    Added new properties.

--- PIMS.Core/Models/Profile.cs
+++ PIMS.Core/Models/Profile.cs
@@ -17,6 +17,10 @@ namespace PIMS.Core.Models
 
         public virtual decimal? DividendRate { get; set; }
 
+        public virtual decimal EarningsPerShare { get; set; }
+
+        public virtual decimal SharePrice { get; set; }
+
         public virtual decimal? DividendYield { get; set; }
 
         public virtual IList<Asset> Security { get; set; }
@@ -24,9 +28,7 @@ namespace PIMS.Core.Models
         // ReSharper disable once InconsistentNaming
         public virtual decimal? PE_Ratio { get; set; }
 
-       
+        public virtual DateTime LastUpdate { get; set; }
 
-        // NHibernate requirement to prevent dirty reads.
-        //public virtual byte[] Version { get; set; }
     }
 }
