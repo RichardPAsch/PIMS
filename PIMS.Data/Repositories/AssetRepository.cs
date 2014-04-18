c9ba6efa014c115b9cb7d2c43996d60dd4f505ee
Richard P. Asch <rpasch@rpclassics.net>
2014-04-18 12:39:32 -0700

    Added InMemoryAssetRepository.

--- PIMS.Data/Repositories/AssetRepository.cs
+++ PIMS.Data/Repositories/AssetRepository.cs
@@ -6,7 +6,7 @@ using PIMS.Core.Models;
 namespace PIMS.Data.Repositories
 {
 
-    public class AssetRepository : IAssetRepository
+    public class AssetRepository : IGenericRepository<Asset>
     {
         //public AssetRepository(string connectionString)
         public AssetRepository()
@@ -16,27 +16,56 @@ namespace PIMS.Data.Repositories
 
 
 
-        public Asset CreateAsset(Asset newAsset)
+        //public Asset CreateAsset(Asset newAsset)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public IQueryable<Asset> FetchAssets()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Asset FetchAsset(long id)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public bool UpdateAsset(long id)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public bool DeleteAsset(long id)
+        //{
+        //    throw new NotImplementedException();
+        //}
+        public IQueryable<Asset> RetreiveAll()
         {
             throw new NotImplementedException();
         }
 
-        public IQueryable<Asset> FetchAssets()
+        public Asset Retreive(object property)
         {
             throw new NotImplementedException();
         }
 
-        public Asset FetchAsset(long id)
+        public Asset RetreiveById(Guid key)
         {
             throw new NotImplementedException();
         }
 
-        public bool UpdateAsset(long id)
+        public bool Create(Asset newEntity)
         {
             throw new NotImplementedException();
         }
 
-        public bool DeleteAsset(long id)
+        public bool Delete(Guid idGuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Asset entity)
         {
             throw new NotImplementedException();
         }
--- PIMS.Data/Repositories/ProfileRepsository.cs
+++ PIMS.Data/Repositories/ProfileRepsository.cs
@@ -8,7 +8,7 @@ using PIMS.Core.Models;
 
 namespace PIMS.Data.Repositories
 {
-    public class ProfileRepository : IProfileRepository
+    public class ProfileRepository : IGenericRepository<Profile>
     {
         public ProfileRepository()
         {
@@ -17,17 +17,47 @@ namespace PIMS.Data.Repositories
 
 
 
-        public Profile CreateProfile(Profile newProfile)
+        //public Profile CreateProfile(Profile newProfile)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Profile FetchProfile(long id)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public bool UpdateProfile(long id)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public IQueryable<Profile> RetreiveAll()
         {
             throw new NotImplementedException();
         }
 
-        public Profile FetchProfile(long id)
+        public Profile Retreive(object property)
         {
             throw new NotImplementedException();
         }
 
-        public bool UpdateProfile(long id)
+        public Profile RetreiveById(Guid key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Create(Profile newEntity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Guid idGuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Profile entity)
         {
             throw new NotImplementedException();
         }
