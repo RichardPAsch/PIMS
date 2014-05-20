8007b90056bafbfd7bf78a2da92229c47096e45f
Richard P. Asch <rpasch@rpclassics.net>
2014-05-20 14:38:17 -0700

    Added/Modified integration tests - tested ok

--- PIMS.IntegrationTest/VerifyAssetClassController.cs
+++ PIMS.IntegrationTest/VerifyAssetClassController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -7,7 +8,6 @@ using System.Net.Http.Headers;
 using Newtonsoft.Json;
 using NUnit.Framework;
 using PIMS.Core.Models;
-using PIMS.Web.Api.Controllers;
 
 
 namespace PIMS.IntegrationTest
@@ -15,6 +15,8 @@ namespace PIMS.IntegrationTest
     [TestFixture]
     public class VerifyAssetClassController
     {
+        
+
         /*
             API tests WebApi endpoints, mimicking client-based calls, as in sending resource-based URL requests; this 
             tests from the client's point of view, and most closely resembles real world scenarios.
@@ -22,13 +24,13 @@ namespace PIMS.IntegrationTest
             These tests also verify IoC/DI is working correctly.
         */
 
-        private AssetClassController _ctrl;
+        //private AssetClassController _ctrl;
         private const string UrlBase = "http://localhost/PIMS.Web.API/api";
       
 
         public void Init()
         {
-            //_sessionFactory = NHibernateConfiguration.CreateSessionFactory(ConnString); // needed?
+           
         }
 
 
@@ -44,7 +46,7 @@ namespace PIMS.IntegrationTest
                     client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
                     // Act
-                    HttpResponseMessage resp = await client.GetAsync(client.BaseAddress);
+                    var resp = await client.GetAsync(client.BaseAddress);
                     var classes = await resp.Content.ReadAsAsync<IEnumerable<AssetClass>>();
 
 
@@ -65,7 +67,7 @@ namespace PIMS.IntegrationTest
 
             using (var client = new HttpClient()) {
                 // Arrange
-                client.BaseAddress = new Uri(UrlBase + "/AssetClass/C3");
+                client.BaseAddress = new Uri(UrlBase + "/AssetClass/ETF");
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
@@ -73,13 +75,65 @@ namespace PIMS.IntegrationTest
                 HttpResponseMessage resp = await client.GetAsync(client.BaseAddress);
                 var assetClass = await resp.Content.ReadAsAsync<AssetClass>();
 
+                
+
                 // Assert
                 Assert.IsTrue(resp.StatusCode == HttpStatusCode.OK);
                 Assert.IsNotNull(assetClass);
-                Assert.IsTrue(assetClass.Code.ToUpper().Trim() == "C3");
+                Assert.IsTrue(assetClass.Code.ToUpper().Trim() == "ETF");
                 Assert.IsFalse(assetClass.Description == string.Empty);
             }
         }
+
+
+        //[Test]
+        //// ReSharper disable once InconsistentNaming
+        //public async void Can_PUT_Update_an_Asset_Classification_By_Asset_Class() {
+
+        //    using (var client = new HttpClient()) {
+
+        //        // Arrange
+        //        client.BaseAddress = new Uri(UrlBase + "/AssetClass/ETF");
+        //        client.DefaultRequestHeaders.Accept.Clear();
+        //        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/octet-stream"));
+        //        var respGet = await client.GetAsync(client.BaseAddress); // async call to server for data
+                
+        //        var updatedAssetClass = new AssetClass
+        //                                {
+        //                                    KeyId = Guid.NewGuid(),
+        //                                    Description = "Exchange-Traded Fund",
+        //                                    Code = "ETF"
+        //                                };
+
+
+        //        //var newContent = new ObjectContent<AssetClass>();
+        //        //newContent.
+        //        //respGet.Content = myObjectContent(updatedAssetClass, new JsonMediaTypeFormatter());
+                
+
+        //        var content = respGet.Content; // write content to a string
+        //        var z = JsonConvert.SerializeObject(content);
+        //        var modifiedAssetClass = JsonConvert.DeserializeObject<AssetClass>(z); // deserialize Json to .Net type
+        //        modifiedAssetClass.Description = "Exchange-Traded Fund"; // made modification
+        //        var jsonModifiedAssetClass = JsonConvert.SerializeObject(modifiedAssetClass); // .Net type to Json string
+        //        var stringContent = new StringContent(jsonModifiedAssetClass);
+              
+                
+
+                
+        //        // Act
+        //        //var respPut = await client.PutAsJsonAsync(client.BaseAddress.ToString(), jsonModifiedAssetClass);
+        //        var respPut = await client.PutAsync(client.BaseAddress.ToString(), stringContent); //respGet.Content);
+        //        var assetClass = await respPut.Content.ReadAsAsync<AssetClass>();
+
+        //        var a = 2;
+        //        // Assert
+        //        Assert.IsTrue(respPut.StatusCode == HttpStatusCode.OK);
+        //        //Assert.IsNotNull(assetClass);
+        //        //Assert.IsTrue(assetClass.Code.ToUpper().Trim() == "ETF");
+        //        Assert.IsTrue(assetClass.Description == "Exchange-Traded Fund");
+        //    }
+        //}
 
 
         [Test]
@@ -88,12 +142,12 @@ namespace PIMS.IntegrationTest
 
             using (var client = new HttpClient()) {
                 // Arrange
-                client.BaseAddress = new Uri(UrlBase + "/AssetClass/C9");
+                client.BaseAddress = new Uri(UrlBase + "/AssetClass/RPA");
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
                 // Act
-                HttpResponseMessage resp = await client.GetAsync(client.BaseAddress);
+                var resp = await client.GetAsync(client.BaseAddress);
                 var assetClass = await resp.Content.ReadAsAsync<AssetClass>();
 
                 // Assert
@@ -108,20 +162,20 @@ namespace PIMS.IntegrationTest
         public async void Can_GET_an_Asset_Classification_By_Asset_Id() {
 
             using (var client = new HttpClient()) {
-                // Arrange
-                client.BaseAddress = new Uri(UrlBase + "/AssetClass/a3cce8b1-a52e-4f1b-8b9d-353b50f0d3bb");
+                // Arrange                                       // b9c7d922-a4d2-49b6-aeb0-a30600f02cc3
+                client.BaseAddress = new Uri(UrlBase + "/AssetClass/b9c7d922-a4d2-49b6-aeb0-a30600f02cc3");
                 client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/octet-stream"));
 
                 // Act
-                HttpResponseMessage resp = await client.GetAsync(client.BaseAddress);
+                var resp = await client.GetAsync(client.BaseAddress);
                 var assetClass = await resp.Content.ReadAsAsync<AssetClass>();
 
                 // Assert
                 Assert.IsTrue(resp.StatusCode == HttpStatusCode.OK);
                 Assert.IsNotNull(assetClass);
-                Assert.IsTrue(assetClass.KeyId == new Guid(" a3cce8b1-a52e-4f1b-8b9d-353b50f0d3bb"));
-                Assert.IsFalse(assetClass.Description == string.Empty);
+                Assert.IsTrue(assetClass.KeyId == new Guid("b9c7d922-a4d2-49b6-aeb0-a30600f02cc3"));
+                Assert.IsFalse(assetClass.Description != "Preferred Stock");
             }
         }
 
@@ -136,9 +190,12 @@ namespace PIMS.IntegrationTest
                 client.BaseAddress = new Uri(UrlBase + "/AssetClass/");
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var random = new Random();
+                var randomNumber = random.Next(0, 100);
+
                 var newClassification = new AssetClass {
-                                                            Code = "tC7",
-                                                            Description = "testDesc7"
+                                                            Code = "t" + randomNumber.ToString(CultureInfo.InvariantCulture),
+                                                            Description = DateTime.Now.ToLocalTime().ToString(CultureInfo.InvariantCulture)
                                                         };
                 
                 // Act
@@ -149,8 +206,8 @@ namespace PIMS.IntegrationTest
 
                 // Assert
                 Assert.IsTrue(response.StatusCode == HttpStatusCode.Created);
-                Assert.IsTrue(classification.Code == "tC7");
-                Assert.NotNull(response.Headers.Location);
+                Assert.IsTrue(classification.Code.Contains("t"));
+                Assert.IsTrue(response.Headers.Location.AbsoluteUri.Contains(classification.Code));
             }
             
         }
