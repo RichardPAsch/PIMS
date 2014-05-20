b594e9a8ed1108783686ad5eedbc29cc3902aa47
Richard P. Asch <rpasch@rpclassics.net>
2014-05-20 14:58:05 -0700

    Modified Update() - to allow for id.

--- PIMS.Data/Repositories/IGenericRepository.cs
+++ PIMS.Data/Repositories/IGenericRepository.cs
@@ -7,14 +7,14 @@ namespace PIMS.Data.Repositories
     public interface IGenericRepository<T> where T : class
     {
         // UoW functionality is covered by NHibernate's ISession.
-
         IQueryable<T> RetreiveAll();
-        //T Retreive(Expression<Func<T, int, bool>> predicate);
         T Retreive(object property);
         T RetreiveById(Guid key);
         bool Create(T newEntity);
         bool Delete(Guid idGuid);
-        bool Update(T entity);
+
+        // Need greater type flexibility for id, i.e., string, Guid.
+        bool Update(T entity, object id);
 
         // bool Save()
     }
