f53e26dd360a7a6072da3a35731615756201d62e
Richard P. Asch <rpasch@rpclassics.net>
2014-05-20 15:06:16 -0700

    Updated for future use.

--- PIMS.Data/Repositories/AssetRepository.cs
+++ PIMS.Data/Repositories/AssetRepository.cs
@@ -8,38 +8,13 @@ namespace PIMS.Data.Repositories
 
     public class AssetRepository : IGenericRepository<Asset>
     {
-        //public AssetRepository(string connectionString)
+
         public AssetRepository()
         {
-            // to be implemented
+            
         }
 
 
-
-        //public Asset CreateAsset(Asset newAsset)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        //public IQueryable<Asset> FetchAssets()
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        //public Asset FetchAsset(long id)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        //public bool UpdateAsset(long id)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        //public bool DeleteAsset(long id)
-        //{
-        //    throw new NotImplementedException();
-        //}
         public IQueryable<Asset> RetreiveAll()
         {
             throw new NotImplementedException();
@@ -65,9 +40,11 @@ namespace PIMS.Data.Repositories
             throw new NotImplementedException();
         }
 
-        public bool Update(Asset entity)
+        public bool Update(Asset entity, object id)
         {
             throw new NotImplementedException();
         }
+
+        
     }
 }
