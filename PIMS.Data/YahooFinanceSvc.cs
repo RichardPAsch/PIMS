e14bf2d1f345bb5c5a818c2ae6fc4b76efeaa067
Richard P. Asch <rpasch@rpclassics.net>
2014-05-20 15:08:55 -0700

    Updated project files.

---
+++ PIMS.Data/YahooFinanceSvc.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+
+
+namespace PIMS.Data
+{
+    internal static class YahooFinanceSvc
+    {
+        public static string Process(string ticker)
+        {
+            string csvProfile;
+
+            using (var web = new WebClient())
+            {
+                csvProfile = web.DownloadString("http://finance.yahoo.com/d/quotes.csv?s=" + ticker + "&f=nsb2dyre");
+            }
+
+            var profile = YahooParser.MapToProfile(csvProfile);
+          
+           return JsonConvert.SerializeObject(profile);
+
+        }
+
+
+    }
+
+}
---
+++ PIMS.Data/YahooParser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using PIMS.Core.Models;
+
+
+namespace PIMS.Data
+{
+    public static class YahooParser
+    {
+        public static Profile MapToProfile(string csvData)
+        {
+            Profile profile = null;
+            var rows = csvData.Replace("\r", "").Split('\n');
+
+            foreach (var newProfile in 
+                    from row in rows 
+                    where !string.IsNullOrEmpty(row) 
+                    select row.Split(',') into cols 
+                    select new Profile
+                                    {
+                                        ProfileId = Guid.NewGuid(),
+                                        TickerSymbol = cols[1].Replace("\"", ""),
+                                        TickerDescription = cols[0].Replace("\"", ""),
+                                        DividendFreq = "M", //TODO - calculate based on payment history?
+                                        DividendRate = cols[3] == "N/A" ? 0 : Convert.ToDecimal(cols[3]),
+                                        EarningsPerShare = cols[6] == "N/A" ? 0 : Convert.ToDecimal(cols[6]),
+                                        SharePrice = cols[2] == "N/A" ? 0 : Convert.ToDecimal(cols[2]),
+                                        DividendYield = cols[4] == "N/A" ? 0 : Convert.ToDecimal(cols[4]),
+                                        PE_Ratio = cols[5] == "N/A" ? 0 : Convert.ToDecimal(cols[5]),
+                                        LastUpdate = DateTime.Now
+                                    }
+                    )
+              
+      
+            {
+                profile = newProfile;
+            }
+
+            return profile;
+        }
+
+    }
+
+
+}
