1045bbb059b87180b618937ff4bbaae2f607adc3
Richard P. Asch <rpasch@rpclassics.net>
2014-07-21 16:58:50 -0700

    Initial creation as part of security implementations.

---
+++ PIMS.Infrastructure/NHibernate/Mappings/AccountMap.cs
@@ -0,0 +1,18 @@
+﻿using FluentNHibernate.Mapping;
+using PIMS.Core.Models;
+
+
+namespace PIMS.Infrastructure.NHibernate.Mappings
+{
+    public class AccountMap : ClassMap<Account>
+    {
+        public AccountMap() {
+            Table("Account");
+            Id(x => x.Id);
+            Map(x => x.AccountPassword, "UserPassword");
+            Map(x => x.AccountPasswordConfirm, "UserPasswordConfirm");
+            Map(x => x.AccountName, "UserName");
+
+        }
+    }
+}
