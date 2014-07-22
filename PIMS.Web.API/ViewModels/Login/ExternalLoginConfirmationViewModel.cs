4b9ae2648251381224a1928d4c4e75a23ffed166
Richard P. Asch <rpasch@rpclassics.net>
2014-07-21 17:27:18 -0700

    Initial creations; may update/remove later.

---
+++ PIMS.Web.API/ViewModels/Login/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PIMS.Web.Api.ViewModels.Login
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+    }
+
+
+
+
+
+
+}
