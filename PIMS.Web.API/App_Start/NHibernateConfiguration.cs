33b1aa27586caf94fcedc08bbe49ff8782ba022c
Richard P. Asch <rpasch@rpclassics.net>
2014-07-21 17:11:07 -0700

    NS cleanup + NH mapping code added.

--- PIMS.Web.API/App_Start/NHibernateConfiguration.cs
+++ PIMS.Web.API/App_Start/NHibernateConfiguration.cs
@@ -1,7 +1,11 @@
 ﻿using FluentNHibernate.Cfg.Db;
 using NHibernate;
 using NHibernate.Context;
+using PIMS.Core.Models;
 using PIMS.Infrastructure.NHibernate.Mappings;
+using NHibernate.AspNet.Identity.Helpers;
+
+
 
 namespace PIMS.Web.Api
 {
@@ -14,11 +18,14 @@ namespace PIMS.Web.Api
             // Use by default, production backend.
             if (connString == string.Empty)
                 connString = @"Data Source=RICHARD-VAIO\RICHARDDB;Initial Catalog='Lighthouse - PIMS';Integrated Security=True";
-          
+
+            // Use default subclassed Identity model.
+            var appUsers = new[] { typeof(ApplicationUser) };
 
             return FluentNHibernate.Cfg.Fluently.Configure()
                 //TODO: Modify to use web.config settings for connection string
                 //.Database(MsSqlConfiguration.MsSql2008.ConnectionString(c => c.FromConnectionStringWithKey("Connection-String"))
+                .ExposeConfiguration(cfg => cfg.AddDeserializedMapping(MappingHelper.GetIdentityMappings(appUsers), null))
                 .Database(MsSqlConfiguration.MsSql2008
                 .ConnectionString(c => c.Is(connString))
                                 //.ShowSql() // temp for testing/debugging.
@@ -35,7 +42,8 @@ namespace PIMS.Web.Api
 
 
         // Bind, if necessary, the current HttpContext to NHibernate's ISession.
-        public static ISession CheckSession(ISessionFactory sf) {
+        public static ISession CheckSession(ISessionFactory sf)
+        {
             if (CurrentSessionContext.HasBind(sf))
                 return sf.GetCurrentSession();
 
