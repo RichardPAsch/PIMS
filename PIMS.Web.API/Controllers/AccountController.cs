ae810563809890cfedb7614e14e0ace8ec3217c4
Richard P. Asch <rpasch@rpclassics.net>
2014-07-24 13:00:23 -0700

    Added LoginAsync() - WIP

--- PIMS.Web.API/Controllers/AccountController.cs
+++ PIMS.Web.API/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -6,16 +7,19 @@ using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using Microsoft.Owin.Security;
+using Newtonsoft.Json.Linq;
 using NHibernate;
 using PIMS.Core.Models;
 using Microsoft.AspNet.Identity;
 
 
+
 namespace PIMS.Web.Api.Controllers
 {
+    [System.Web.Http.RoutePrefix("api/Account")]
     public class AccountController : ApiController
     {
-
+        private const string UrlBase = "http://localhost/PIMS.Web.Api";
         public UserManager<ApplicationUser> UserManager { get; private set; }
         public object SignInStub { get; set; }
         private static ISessionFactory _sf;
@@ -61,7 +65,6 @@ namespace PIMS.Web.Api.Controllers
 
         
         private async Task SignInAsync(ApplicationUser user, bool isPersistent) {
-
             try
             {
                 AuthenticationManager.SignOut(DefaultAuthenticationTypes.ExternalCookie);
@@ -86,6 +89,8 @@ namespace PIMS.Web.Api.Controllers
         [System.Web.Http.HttpPost]
         [System.Web.Http.AllowAnonymous]
         [ValidateAntiForgeryToken]
+        // Include "action" in route template to avoid Http 500: "Multiple actions were found that match the request".
+        [System.Web.Http.Route("RegisterAsync")]
         public async Task<IHttpActionResult> RegisterAsync([FromBody] RegistrationModel registrationData) {
 
             if (!ModelState.IsValid) return BadRequest("Invalid registration data received.");
@@ -112,27 +117,47 @@ namespace PIMS.Web.Api.Controllers
         }
 
 
-            
+
+        // POST: /Account/LoginAsync
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        [System.Web.Http.Route("LoginAsync")]
+        public async Task<string> LoginAsync([FromBody] LoginModel loginData)
+        {
+            if (!ModelState.IsValid) return "Invalid login or unregistered.";
+
+            using (var client = new HttpClient())
+            {
+                var login = new Dictionary<string, string>
+                           {
+                               {"grant_type", "password"},
+                               {"username", loginData.UserName},
+                               {"password", loginData.Password}
+                           };
+
+                var response = await client.PostAsync(UrlBase + "/token", new FormUrlEncodedContent(login));
+                var content = await response.Content.ReadAsStringAsync();
+
+                var json = JObject.Parse(content);
+                return json["access_token"].ToString();
+            }
+
+           
 
 
-        
+            //var user = await UserManager.FindAsync(model.UserName, model.Password);
+            //if (user != null) {
+            //    await SignInAsync(user, model.RememberMe);
+            //    return Ok();
+            //}
+            //ModelState.AddModelError("", "Invalid username and/or password.");
 
-        //// POST: /Account/Login
-        //[System.Web.Http.HttpPost]
-        //[System.Web.Http.AllowAnonymous]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IHttpActionResult> Login(LoginViewModel model, string returnUrl) {
-        //    if (!ModelState.IsValid) return Unauthorized();
-        //    var user = await UserManager.FindAsync(model.UserName, model.Password);
-        //    if (user != null) {
-        //        await SignInAsync(user, model.RememberMe);
-        //        return Ok();
-        //    }
-        //    ModelState.AddModelError("", "Invalid username and/or password.");
+            // If we got this far, something failed, redisplay form
+            //return StatusCode(HttpStatusCode.Unauthorized);
+        }
 
-        //    // If we got this far, something failed, redisplay form
-        //    return StatusCode(HttpStatusCode.Unauthorized);
-        //}
+
 
         //private void AddErrors(IdentityResult result) {
         //    foreach (var error in result.Errors) {
