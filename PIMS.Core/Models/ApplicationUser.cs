b6d1f51706c0d4e5c0d96120a07e94b05a76d7a9
Richard P. Asch <rpasch@rpclassics.net>
2014-07-24 13:03:33 -0700

    File changes.

--- PIMS.Core/Models/ApplicationUser.cs
+++ PIMS.Core/Models/ApplicationUser.cs
@@ -8,9 +8,11 @@ namespace PIMS.Core.Models
     // Additional profile data (e.g., birthDay) for the user can be added to this class; 
     // please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
 
+    // "ApplicationUser" - do not change name, as it maps to corresponding db table, and will result in run-time error.
+
     public class ApplicationUser : IdentityUser
     {
-        // "ApplicationUser" - do not change name, as it results in run-time error.
+        
     }
 
 
