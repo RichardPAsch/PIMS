2d8aed07338f66d5e22af1b8ca890e2d2aba4f8d
Richard P. Asch <rpasch@rpclassics.net>
2014-09-03 13:46:41 -0700

    Modified auth server (Katana).

--- PIMS.Core/Models/ApplicationUser.cs
+++ PIMS.Core/Models/ApplicationUser.cs
@@ -5,14 +5,22 @@ namespace PIMS.Core.Models
 {
     // ** Extensible part of new ASP.NET Identity membership system, as used by OWIN/Katana middleware. **
 
-    // Additional profile data (e.g., birthDay) for the user can be added to this class; 
-    // please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    /*
+     * Additional profile data (e.g., birthDay) for the user can be added to this class; 
+     * please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    */ 
 
     // "ApplicationUser" - do not change name, as it maps to corresponding db table, and will result in run-time error.
 
+
+
+    // Extend the functionality of the default IdentityUser entity as needed:
+    //  1. extend the corresponding view model 
+    //  2. add new attribute(s) to UI for initialization(s)
+    //  3. add new attributes(s) to AspNetUsers
     public class ApplicationUser : IdentityUser
     {
-        
+        //public string FavoriteMovie { get; set; }
     }
 
 
