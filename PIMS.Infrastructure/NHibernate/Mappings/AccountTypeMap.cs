530093560f8ba671ceb3be325d3a31265b29f7f9
Richard P. Asch <rpasch@rpclassics.net>
2014-12-08 10:31:13 -0800

    Updated for unit testing.

---
+++ PIMS.Infrastructure/NHibernate/Mappings/AccountTypeMap.cs
@@ -0,0 +1,18 @@
+﻿using FluentNHibernate.Mapping;
+using PIMS.Core.Models;
+
+
+namespace PIMS.Infrastructure.NHibernate.Mappings
+{
+    public class AccountTypeMap : ClassMap<AccountType>
+    {
+        public AccountTypeMap()
+        {
+            Id(x => x.KeyId, "AccountTypeId");
+            Map(x => x.AccountTypeDesc, "AccountType");
+            Table("AccountType");
+
+        }
+
+    }
+}
