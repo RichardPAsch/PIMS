23098c01409de4ca6c2d2d0dc50f2704fc64d8b7
Richard P. Asch <rpasch@rpclassics.net>
2014-12-08 10:44:32 -0800

    Initial creation.

---
+++ PIMS.Infrastructure/NHibernate/Mappings/InvestorMap.cs
@@ -0,0 +1,31 @@
+﻿using FluentNHibernate.Mapping;
+using PIMS.Core.Models;
+
+
+namespace PIMS.Infrastructure.NHibernate.Mappings
+{
+    public class InvestorMap : ClassMap<Investor>
+    {
+        public InvestorMap() {
+            Table("Investor");
+            Id(x => x.InvestorId);
+            Map(x => x.FirstName);
+            Map(x => x.LastName);
+            Map(x => x.Address1);
+            Map(x => x.Address2);
+            Map(x => x.City);
+            Map(x => x.ZipCode);
+            Map(x => x.Phone);
+            Map(x => x.Mobile);
+            Map(x => x.AspNetUsersId);
+            Map(x => x.EMailAddr);
+            Map(x => x.BirthDay);
+
+            //HasMany(x => x.Security).Cascade.DeleteOrphan().Inverse();
+
+
+
+
+        }
+    }
+}
