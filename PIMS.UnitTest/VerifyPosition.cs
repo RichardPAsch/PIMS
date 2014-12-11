78f4bcee8e271500b3727731fa3aa346251cf705
Richard P. Asch <rpasch@rpclassics.net>
2014-12-10 17:38:53 -0800

    Updated for POSTing tests. ok per Fiddler

--- PIMS.UnitTest/VerifyPosition.cs
+++ PIMS.UnitTest/VerifyPosition.cs
@@ -1,14 +1,15 @@
 ﻿using System;
-using System.Net;
+using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
-using Newtonsoft.Json;
+using System.Web.Http.Results;
 using NUnit.Framework;
+using PIMS.Core.Models;
+using PIMS.Core.Security;
 using PIMS.Data.FakeRepositories;
 using PIMS.Web.Api.Controllers;
 using Moq;
-using PIMS.Core.Models;
-
 
 
 namespace PIMS.UnitTest
@@ -18,198 +19,194 @@ namespace PIMS.UnitTest
     {
         private PositionController _ctrl;
         private Mock<InMemoryPositionRepository> _mockRepo;
-        private const string UrlBase = "http://localhost/PIMS.Web.API/api/Position";
+        private const string UrlBase = "http://localhost/PIMS.Web.API/api/Asset/<ticker>/Position";
+        private Mock<PimsIdentityService> _mockIdentitySvc;
+        private Mock<InMemoryAssetRepository> _mockRepoAsset;
 
+
+        //----------- Menu BUSINESS RULES : per Investor ---------------------------------------------------------------------------------
+        //  Asset/Create - One or more Positions can be created client-side, and POSTed as an aggregate at time of initial Asset creation.
+        //                 See VerifyAsset.
+        //  Position/Create - Adds new Position to existing Asset.
+        //  Position/Retreive-Update - Adds to or modifies existing Position. Each Position is uniquely identified by its' Account type.
+        //  Position/Delete - Removes appropriate Position(s) per Asset.
+        //
+        //---------------------------------------------------------------------------------------------------------------------------------
+
+        // TODO: (C)reate - done,  (R)etreive - done 12/10
        
         [SetUp]
         public void Init()
         {
             _mockRepo = new Mock<InMemoryPositionRepository>();
+            _mockIdentitySvc = new Mock<PimsIdentityService>();
+            _mockRepoAsset = new Mock<InMemoryAssetRepository>();
         }
 
-
-        //[Test]
-        //// ReSharper disable once InconsistentNaming
-        //public void Controller_can_GET_a_single_Fake_Profile_for_a_valid_ticker_symbol() {
-
-        //    // Arrange 
-        //    var request = TestHelpers.GetHttpRequestMessage();
-        //    _ctrl = new ProfileController(_mockRepo.Object);
-
-        //    // Act
-        //    var myProfile = _ctrl.Get(request, "HMC");
-        //    var taskProfile = myProfile.Content.ReadAsAsync<Profile>().Result;
-
-
-        //    // Assert
-        //    Assert.IsNotNull(myProfile);
-        //    Assert.IsTrue(HttpStatusCode.OK == myProfile.StatusCode);
-        //    Assert.IsTrue(taskProfile.TickerSymbol == "HMC");
-        //}
-
-        //[Test]
-        //// ReSharper disable once InconsistentNaming
-        //public void Controller_cannot_GET_a_single_Fake_Profile_for_a_invalid_ticker_symbol() {
-
-        //    // Arrange 
-        //    var request = TestHelpers.GetHttpRequestMessage();
-        //    _ctrl = new ProfileController(_mockRepo.Object);
-
-        //    // Act
-        //    var myProfile = _ctrl.Get(request, "RPA");
-        //    var taskProfile = myProfile.Content.ReadAsAsync<Profile>().Result;
-
-
-        //    // Assert
-        //    Assert.IsTrue(myProfile.StatusCode == HttpStatusCode.NotFound);
-        //    Assert.IsNull(taskProfile.TickerSymbol);
-        //}
-
-        //[Test]
-        //// ReSharper disable once InconsistentNaming
-        //public void Controller_can_GET_a_single_Fake_Profile_based_on_a_valid_AssetId() {
-
-        //    // Arrange 
-        //    var request = TestHelpers.GetHttpRequestMessage();
-        //    _ctrl = new ProfileController(_mockRepo.Object);
-
-        //    // Act
-        //    var myProfile = _ctrl.Get(request, new Guid("e07a582a-aec8-43b9-9cb8-faed5e5434de"));
-        //    var taskProfile = myProfile.Content.ReadAsAsync<Profile>().Result;
-
-
-        //    // Assert
-        //    Assert.IsNotNull(myProfile);
-        //    Assert.IsTrue(HttpStatusCode.OK == myProfile.StatusCode);
-        //    Assert.IsTrue(taskProfile.TickerSymbol == "GSK");
-        //}
 
         [Test]
         // ReSharper disable once InconsistentNaming
-        public void Controller_can_POST_a_new_fake_Position_for_a_given_users_asset() {
+        public async Task Controller_Can_GET_a_single_fake_position_for_an_Asset_via_ticker_symbol_and_account() {
 
-            //// Arrange 
-            //_ctrl = new ProfileController(_mockRepo.Object);
-            //var newProfile = new Profile() {
-            //    AssetId = Guid.NewGuid(),
-            //    UnitPrice = 692.16M,
-            //    DividendFreq = "S",
-            //    DividendYield = 3.07M,
-            //    DividendRate = 1.052M,
-            //    TickerDescription = "Google",
-            //    EarningsPerShare = 7.96M,
-            //    PE_Ratio = 19.01M,
-            //    LastUpdate = DateTime.Now,
-            //    TickerSymbol = "GOOG"
-            //};
+            // Arrange - SUT
+            _ctrl = new PositionController(_mockRepo.Object, _mockIdentitySvc.Object, _mockRepoAsset.Object)
+            {
+                Request = new HttpRequestMessage { RequestUri = new Uri("http://localhost/PIMS.Web.Api/api/Asset/AAPL/Position/Roth-IRA") },
+                Configuration = new HttpConfiguration()
+            };
 
-            //// Act
-            //var result = _ctrl.Post(newProfile, TestHelpers.GetHttpRequestMessage(
-            //                                                HttpMethod.Post,
-            //                                                UrlBase + "/" + newProfile.TickerSymbol + "/Profile",
-            //                                                _ctrl,
-            //                                                "ProfileRoute",
-            //                                                "api/Asset/{ticker}/Profile/{ProfileId}",
-            //                                                new { ProfileId = RouteParameter.Optional }
-            //                                            ));
-            //// Format entity.
-            //var jsonResult = result.Content.ReadAsStringAsync().Result;
-            //var profileEntity = JsonConvert.DeserializeObject<Profile>(jsonResult);
+            // Act 
+            var assetPosition = await _ctrl.GetPositionByAccount("AAPL", "Roth-IRA") as OkNegotiatedContentResult<IQueryable<Position>>;
 
 
-            //// Assert
-            //Assert.AreEqual(HttpStatusCode.Created, result.StatusCode);
-            //Assert.IsNotNull(profileEntity);
-            //// TODO: Use relative path ?
-            //Assert.AreEqual(result.Headers.Location, UrlBase + "/" + newProfile.TickerSymbol + "/Profile");
-            //Assert.IsTrue(profileEntity.DividendFreq == "S");
-            //Assert.IsTrue(profileEntity.AssetId != Guid.Empty);
+            // Assert
+            Assert.IsNotNull(assetPosition);
+            Assert.That(assetPosition.Content.Count(), Is.EqualTo(1));
+            Assert.That(assetPosition.Content.First().Account.AccountTypeDesc, Is.EqualTo("Roth-IRA"));
+          
         }
 
-        //[Test]
-        //// ReSharper disable once InconsistentNaming
-        //public void Controller_cannot_POST_a_new_duplicate_fake_Profile() {
 
-        //    // Arrange 
-        //    _ctrl = new ProfileController(_mockRepo.Object);
-        //    var newProfile = new Profile() {
-        //        AssetId = new Guid("e07a582a-aec8-43b9-9cb8-faed5e5434de"),
-        //        UnitPrice = 22.16M,
-        //        DividendFreq = "M",
-        //        DividendYield = 4.892M,
-        //        DividendRate = .912M,
-        //        TickerDescription = "Glaxo Smith Kline",
-        //        EarningsPerShare = 3.46M,
-        //        PE_Ratio = 15.17M,
-        //        LastUpdate = DateTime.Now,
-        //        TickerSymbol = "GSK"
-        //    };
+        [Test]
+        // ReSharper disable once InconsistentNaming
+        public async Task Controller_Can_GET_all_fake_positions_for_an_Asset_via_ticker_symbol() {
 
-        //    // Act
-        //    var result = _ctrl.Post(newProfile, TestHelpers.GetHttpRequestMessage(
-        //                                                    HttpMethod.Post,
-        //                                                    UrlBase + "/" + newProfile.TickerSymbol + "/Profile",
-        //                                                    _ctrl,
-        //                                                    "ProfileRoute",
-        //                                                    "api/Asset/{ticker}/Profile/{ProfileId}",
-        //                                                    new { ProfileId = RouteParameter.Optional }
-        //                                                ));
-        //    // Format entity.
-        //    var jsonResult = result.Content.ReadAsStringAsync().Result;
-        //    var profileEntity = JsonConvert.DeserializeObject<Profile>(jsonResult);
+            // Arrange - SUT
+            _ctrl = new PositionController(_mockRepo.Object, _mockIdentitySvc.Object, _mockRepoAsset.Object) {
+                Request = new HttpRequestMessage { RequestUri = new Uri("http://localhost/PIMS.Web.Api/api/Asset/AAPL/Position") },
+                Configuration = new HttpConfiguration()
+            };
+
+            // Act 
+            var assetPositions = await _ctrl.GetPositionsByAsset("AAPL") as OkNegotiatedContentResult<IQueryable<Position>>;
 
 
-        //    // Assert
-        //    Assert.AreEqual(HttpStatusCode.Conflict, result.StatusCode); // 409 status code
-        //    Assert.IsNotNull(profileEntity);
-        //    Assert.IsTrue(profileEntity.AssetId == Guid.Empty);
-        //}
+            // Assert
+            Assert.IsNotNull(assetPositions);
+            Assert.That(assetPositions.Content.Count(), Is.GreaterThanOrEqualTo(3));
+            Assert.That(assetPositions.Content.Last().Account.AccountTypeDesc, Is.EqualTo("Roth-IRA"));
+
+        }
 
 
-        //[Test]
-        //// ReSharper disable once InconsistentNaming
-        //public void Controller_can_Update_PUT_a_fake_Profile() {
+        [Test]
+        // ReSharper disable once InconsistentNaming
+        public async Task Controller_can_not_POST_a_new_duplicate_fake_Position_for_a_given_users_asset() {
 
-        //    // Arrange - edited
-        //    var revisedProfile = new Profile() {
-        //        AssetId = new Guid("dbf1590e-b99f-468d-a44e-e5e8f86c5f86"),
-        //        UnitPrice = 201.06M,
-        //        DividendFreq = "Q",
-        //        DividendYield = 6.79M,
-        //        DividendRate = 1.31M,
-        //        TickerDescription = "International Business Machines",
-        //        EarningsPerShare = 6.81M,
-        //        PE_Ratio = 21.07M,
-        //        LastUpdate = DateTime.Now,
-        //        TickerSymbol = "IBM"
-        //    };
+            // Arrange - SUT
+            _ctrl = new PositionController(_mockRepo.Object, _mockIdentitySvc.Object, _mockRepoAsset.Object) {
+                                    Request = new HttpRequestMessage { RequestUri = new Uri("http://localhost/PIMS.Web.Api/api/Asset/IBM/Position") },
+                                    Configuration = new HttpConfiguration()
+            };
 
-        //    var request = TestHelpers.GetHttpRequestMessage();
-        //    _ctrl = new ProfileController(_mockRepo.Object);
+            var newPosition = new Position
+                                {
+                                    Url = "http://localhost/PIMS.Web.Api/api/Asset/IBM/Position",
+                                    PositionId = Guid.NewGuid(), 
+                                    PurchaseDate = DateTime.UtcNow.ToString("d"), 
+                                    Quantity = 109,
+                                    UnitCost = 162.99M,
+                                    LastUpdate = DateTime.UtcNow.ToString("g"),
+                                    Account = new AccountType
+                                            {
+                                                Url = "http://localhost/PIMS.Web.Api/api/Asset/AAPL/Position/Account/IRRA",
+                                                AccountTypeDesc = "Roth-IRA",
+                                                KeyId = Guid.NewGuid()
+                                            }
+                                };
 
-        //    // Act
-        //    var respMsg = _ctrl.Put(revisedProfile, request);
+            //var debugJsonForFiddler = TestHelpers.ObjectToJson(newPosition);
+
+            // Act
+            var assetPosition = await _ctrl.CreateNewPosition(newPosition) as OkNegotiatedContentResult<IQueryable<Position>>;
 
 
-        //    // Assert
-        //    Assert.AreEqual(HttpStatusCode.OK, respMsg.StatusCode);
-        //}
+            // Assert
+            Assert.IsNull(assetPosition);
+          
+        }
 
-        //[Test]
-        //// ReSharper disable once InconsistentNaming
-        //public void Controller_can_DELETE_a_single_fake_Profile() {
 
-        //    // TODO - per ADMIN role only!
-        //    // Arrange 
-        //    var request = TestHelpers.GetHttpRequestMessage();
-        //    _ctrl = new ProfileController(_mockRepo.Object);
+        [Test]
+        // ReSharper disable once InconsistentNaming
+        public async Task Controller_can_POST_an_additional_fake_Position_for_an_existing_asset() {
 
-        //    // Act
-        //    var result = _ctrl.Delete(request, new Guid("e07a582a-aec8-43b9-9cb8-faed5e5434de"));
+            // Arrange - SUT
+            _ctrl = new PositionController(_mockRepo.Object, _mockIdentitySvc.Object, _mockRepoAsset.Object) {
+                Request = new HttpRequestMessage { RequestUri = new Uri("http://localhost/PIMS.Web.Api/api/Asset/VNR/Position") },
+                Configuration = new HttpConfiguration()
+            };
 
-        //    // Assert
-        //    Assert.IsTrue(result.StatusCode == HttpStatusCode.OK);
-        //}
+            var newAcctId = Guid.NewGuid();
+            var newPosition = new Position {
+                Url = "http://localhost/PIMS.Web.Api/api/Asset/VNR/Position/Roth-IRA",
+                PositionId = Guid.NewGuid(),
+                PurchaseDate = DateTime.UtcNow.ToString("d"),
+                LastUpdate = DateTime.UtcNow.ToString("g"),
+                Quantity = 500,
+                UnitCost = 17.05M,
+                Account = new AccountType {
+                    Url = "http://localhost/PIMS.Web.Api/api/Asset/VNR/Position/Account/" + newAcctId,
+                    AccountTypeDesc = "Roth-IRA",
+                    KeyId = Guid.NewGuid()
+                }
+            };
+
+            //var debugJsonForFiddler = TestHelpers.ObjectToJson(newPosition);
+
+            // Act
+            var positionActionResult = await _ctrl.CreateNewPosition(newPosition) as CreatedNegotiatedContentResult<Position>;
+            
+
+            // Assert
+            Assert.IsNotNull(positionActionResult);
+            Assert.That(positionActionResult.Content.Account.AccountTypeDesc, Is.EqualTo("Roth-IRA"));
+            Assert.That(positionActionResult.Location.AbsoluteUri, Is.EqualTo("http://localhost/PIMS.Web.Api/api/Asset/VNR/Position/Roth-IRA"));
+            Assert.That(positionActionResult.Content.Quantity, Is.EqualTo(500));
+
+        }
+
+
+        [Test]
+        // ReSharper disable once InconsistentNaming
+        public async Task Controller_can_POST_a_new_fake_Position_as_part_of_the_new_asset_creation_process()
+        {
+            // Arrange -  
+            _ctrl = new PositionController(_mockRepo.Object, _mockIdentitySvc.Object, _mockRepoAsset.Object) {
+                Request = new HttpRequestMessage { RequestUri = new Uri("http://localhost/PIMS.Web.Api/api/Asset/PFE/Position?newAsset=true") },
+                Configuration = new HttpConfiguration()
+            };
+
+            var newAcctId = Guid.NewGuid();
+            var newPosition = new Position {
+                Url = "http://localhost/PIMS.Web.Api/api/Asset/PFE/Position/ML-CMA",
+                PositionId = Guid.NewGuid(),
+                PurchaseDate = DateTime.UtcNow.ToString("d"),
+                LastUpdate = DateTime.UtcNow.ToString("g"),
+                Quantity = 250,
+                UnitCost = 31.86M,
+                Account = new AccountType {
+                    Url = "http://localhost/PIMS.Web.Api/api/Asset/PFE/Position/Account/" + newAcctId,
+                    AccountTypeDesc = "ML-CMA",
+                    KeyId = Guid.NewGuid()
+                }
+            };
+
+            //var debugJsonForFiddler = TestHelpers.ObjectToJson(newPosition);
+
+            // Act
+            var positionActionResult = await _ctrl.CreateNewPosition(newPosition, true) as CreatedNegotiatedContentResult<Position>;
+
+
+            // Assert
+            Assert.IsNotNull(positionActionResult);
+            Assert.That(positionActionResult.Content.Account.AccountTypeDesc, Is.EqualTo("ML-CMA"));
+            Assert.That(positionActionResult.Location.AbsoluteUri, Is.EqualTo("http://localhost/PIMS.Web.Api/api/Asset/PFE/Position/ML-CMA"));
+            Assert.That(positionActionResult.Content.Quantity, Is.EqualTo(250));
+
+        }
+
+        
 
 
 
