7cbe196d0a24d421767b49479ae2468c6e886ff3
Richard P. Asch <rpasch@rpclassics.net>
2015-01-13 15:52:03 -0800

    Initial creation

---
+++ PIMS.Core/Models/ViewModels/QueryResultsVim.cs
@@ -0,0 +1,23 @@
+﻿namespace PIMS.Core.Models.ViewModels
+{
+    // NOTE: Class names closely correspond to Income controller action names.
+
+    public class AssetRevenueForLastTwoYearsVm
+    {
+        // View model for housing various "Income" querying option results.
+        public string Ticker { get; set; }
+        public string DateReceived { get; set; }
+        public decimal AmountReceived { get; set; }
+        
+    }
+
+    public class AssetsBasedOnYieldsVm
+    {
+        // View model for housing Profile dividend data results.
+        public string Ticker { get; set; }
+        public string TickerDescription { get; set; }
+        public decimal DividendYield { get; set; }
+        public decimal DividendRate { get; set; }
+
+    }
+}
