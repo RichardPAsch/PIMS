b990f9c1bb6d651235754c0c222c25808b394c49
Richard P. Asch <rpasch@rpclassics.net>
2015-01-13 16:09:23 -0800

    Introduced Aggregate Root concept.

--- PIMS.Data/Repositories/IGenericRepository.cs
+++ PIMS.Data/Repositories/IGenericRepository.cs
@@ -7,22 +7,19 @@ namespace PIMS.Data.Repositories
 {
     public interface IGenericRepository<T> where T : class
     {
+        // Aggregate root.
+
         // UoW functionality is covered by NHibernate's ISession.
+
+        string UrlAddress { get; set; }
+
         IQueryable<T> RetreiveAll();
-        IQueryable<T> Retreive(Expression<Func<T, bool>> predicate, IQueryable<object> data = null); 
+        IQueryable<T> Retreive(Expression<Func<T, bool>> predicate); 
         T RetreiveById(Guid key);
         bool Create(T newEntity);
         bool Delete(Guid idGuid);
         bool Update(T entity, object id);
-
-        string UrlAddress { get; set; }
-
-        // Use-case scenarios:
-        // Retreive Assets by Investor
-        // Retreive Classification by Asset
-        // Retreive Profile by Asset
-        // Retreive Positions by Investor.Assets
-        // Retreive Incomes by Investor.Assets
         
     }
+
 }
