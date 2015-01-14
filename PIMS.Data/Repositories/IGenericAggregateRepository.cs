477ed48f94c59ec334a888948208ecb11c1d18b5
Richard P. Asch <rpasch@rpclassics.net>
2015-01-13 16:12:14 -0800

    Updated project files.

---
+++ PIMS.Data/Repositories/IGenericAggregateRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace PIMS.Data.Repositories
+{
+    public interface IGenericAggregateRepository
+    {
+        // Handles working with Aggregate Roots (AssetRepository) where CRUD issues surrounding child aggregates, e.g., Position, Income, or Profile,
+        // and their repositories, now have their responsibilities delegated to the parent root aggregate, avoiding circular references & complicated
+        // references among child repositories.
+
+        bool AggregateCreate<T>(T newEntity, string investor, string ticker);
+        bool AggregateDelete<T>(Guid keyId);
+        bool AggregateUpdate<T>(T editedEntity, string investor, string ticker);
+    }
+}
