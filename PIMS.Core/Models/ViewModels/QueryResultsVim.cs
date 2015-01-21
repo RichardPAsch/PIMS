dbbf4c296b37a217b2dac63940e7cb8dde327184
Richard P. Asch <rpasch@rpclassics.net>
2015-01-21 11:44:02 -0800

    Updated for Income unit testing.

--- PIMS.Core/Models/ViewModels/QueryResultsVim.cs
+++ PIMS.Core/Models/ViewModels/QueryResultsVim.cs
@@ -2,22 +2,54 @@
 {
     // NOTE: Class names closely correspond to Income controller action names.
 
-    public class AssetRevenueForLastTwoYearsVm
+    public class AssetRevenueVm
     {
         // View model for housing various "Income" querying option results.
         public string Ticker { get; set; }
         public string DateReceived { get; set; }
         public decimal AmountReceived { get; set; }
+    }
+
+   
+    public class TotalRevenueAndPymtFreqByAssetVm
+    {
+        // View model for housing all asset revenue to date.
+        public string Ticker { get; set; }
+        public string DatePurchased { get; set; }
+        public decimal TotalIncome { get; set; }
+        public string IncomeFrequency { get; set; }
+    }
+
+    public class RevenueYtdVm
+    {
+        public int MonthRecvd { get; set; }
+        public decimal AmountRecvd { get; set; }
+        public decimal YtdAverage { get; set; }
+        public decimal Rolling3MonthAverage { get; set; }
+
+        public decimal TestMethod()
+        {
+            return 100M;
+        }
+
         
     }
 
-    public class AssetsBasedOnYieldsVm
+    public class AssetProfilesVm
     {
-        // View model for housing Profile dividend data results.
         public string Ticker { get; set; }
         public string TickerDescription { get; set; }
         public decimal DividendYield { get; set; }
         public decimal DividendRate { get; set; }
-
     }
-}
+
+    public class RevenuePymtFreqVm
+    {
+        public string TickerSymbol { get; set; }
+        public string IncomeFrequency { get; set; }
+        public decimal TotalRecvd { get; set; }
+    }                                                                  
+                                                                           
+ }
+
+
