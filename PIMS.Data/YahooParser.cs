4bd451a72934623d15c4d610bc0ffaf34fde95ad
Richard P. Asch <rpasch@rpclassics.net>
2015-02-10 15:18:21 -0800

    Corrected MapToProfile() errors.

--- PIMS.Data/YahooParser.cs
+++ PIMS.Data/YahooParser.cs
@@ -12,23 +12,27 @@ namespace PIMS.Data
             Profile profile = null;
             var rows = csvData.Replace("\r", "").Split('\n');
 
+            // Yahoo params: &f=n s b2 dyreqr1
             foreach (var newProfile in 
                     from row in rows 
                     where !string.IsNullOrEmpty(row) 
                     select row.Split(',') into cols 
                     select new Profile
                                     {
-                                        AssetId = Guid.NewGuid(),
+
+
+                                        AssetId           = Guid.NewGuid(),
                                         TickerDescription = cols[0].Replace("\"", ""),
-                                        TickerSymbol = cols[1].Replace("\"", ""),
-                                        DividendFreq = "TBD", //TODO - calculate based on payment history?
-                                        DividendRate = cols[3] == "N/A" ? 0 : Convert.ToDecimal(cols[3]),
-                                        DividendYield = cols[4] == "N/A" ? 0 : Convert.ToDecimal(cols[4]),
-                                        PE_Ratio = cols[5] == "N/A" ? 0 : Convert.ToDecimal(cols[5]),
-                                        EarningsPerShare = cols[6] == "N/A" ? 0 : Convert.ToDecimal(cols[6]),
-                                        ExDividendDate = cols[7] == "N/A" ? new DateTime(1900, 1, 1).ToString("d") : (cols[7]),
-                                        DividendPayDate = cols[8] == "N/A" ? new DateTime(1900, 1, 1).ToString("d") : (cols[8]),
-                                        LastUpdate = DateTime.UtcNow.ToString("g")
+                                        TickerSymbol      = cols[1].Replace("\"", ""),
+                                        Price             = cols[2] == "N/A" ? 0 : Convert.ToDecimal(cols[2]),
+                                        DividendRate      = cols[3] == "N/A" ? 0 : Convert.ToDecimal(cols[3]),
+                                        DividendYield     = cols[4] == "N/A" ? 0 : Convert.ToDecimal(cols[4]),
+                                        PE_Ratio          = cols[5] == "N/A" ? 0 : Convert.ToDecimal(cols[5]),
+                                        EarningsPerShare  = cols[6] == "N/A" ? 0 : Convert.ToDecimal(cols[6]),
+                                        ExDividendDate    = cols[7] == "N/A" ? new DateTime(1900, 1, 1).ToString("d") : (cols[7]),
+                                        DividendPayDate   = cols[8] == "N/A" ? new DateTime(1900, 1, 1).ToString("d") : (cols[8]),
+                                        LastUpdate        = DateTime.UtcNow.ToString("g"),
+                                        DividendFreq      = "TBD", //TODO - calculate based on payment history?
                                     }
                     )
               
