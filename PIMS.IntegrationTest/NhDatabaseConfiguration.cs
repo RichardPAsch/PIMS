d2a5f4655c6b90d57420b664c849a7346009ff2b
Richard P. Asch <rpasch@rpclassics.net>
2015-04-10 14:15:22 -0700

    Added Infrastructure mappings; to revisit need.

--- PIMS.IntegrationTest/NhDatabaseConfiguration.cs
+++ PIMS.IntegrationTest/NhDatabaseConfiguration.cs
@@ -1,7 +1,6 @@
 ﻿using FluentNHibernate.Cfg.Db;
 using NHibernate;
 using PIMS.Infrastructure.NHibernate.Mappings;
-using System.Configuration;
 
 
 namespace PIMS.IntegrationTest
@@ -10,23 +9,26 @@ namespace PIMS.IntegrationTest
     {
         public static ISessionFactory CreateSessionFactory()
         {
-            // Using test database.
 
             // TODO: Note use of NHibernate ApplicationUser setup in PIMS.Web.Api.NHibernateConfiguration; replicate here ?
-            var testConnString = ConfigurationManager.ConnectionStrings["PIMS-ConnString"].ConnectionString;
+            // TODO: can't read from Web.config ??
+            //var testConnString = ConfigurationManager.ConnectionStrings["PIMS-ConnString"].ConnectionString;
+
             return FluentNHibernate.Cfg.Fluently.Configure()
                 .Database(MsSqlConfiguration.MsSql2008
-                .ConnectionString(c => c.Is(testConnString)))
-                //.ConnectionString(c => c.Is(@"Data Source=RICHARD-VAIO\RICHARDDB;Initial Catalog='Lighthouse - PIMS - Test';Integrated Security=True")))
+                //.ConnectionString(c => c.Is(testConnString)))
+                .ConnectionString(c => c.Is(@"Data Source=RICHARD-VAIO\RICHARDDB;Initial Catalog='Lighthouse - PIMS - Test';Integrated Security=True")))
                 .CurrentSessionContext("web")
-                 // Using mappings from WebApi. 
-                 // TODO: Should we use our own /Test/Mappings instead ?
+
+                 // PIMS.Infrastructure mappings.
+                 //TODO: Needed ?
                 .Mappings(m => m.FluentMappings.AddFromAssemblyOf<IncomeMap>())
                 .Mappings(m => m.FluentMappings.AddFromAssemblyOf<ProfileMap>())
                 .Mappings(m => m.FluentMappings.AddFromAssemblyOf<PositionMap>())
                 .Mappings(m => m.FluentMappings.AddFromAssemblyOf<AssetMap>())
                 .Mappings(m => m.FluentMappings.AddFromAssemblyOf<AccountTypeMap>())
                 .Mappings(m => m.FluentMappings.AddFromAssemblyOf<InvestorMap>())
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<AssetClassMap>())
                 .BuildSessionFactory();
 
         }
