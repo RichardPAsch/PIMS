e58e1ff3ff48d7f2d3ed5f930b40060023314cec
Richard P. Asch <rpasch@rpclassics.net>
2015-07-09 17:11:01 -0700

    Added GetInvestorId() & GetBaseUrl() .

--- PIMS.Web.API/Common/Utilities.cs
+++ PIMS.Web.API/Common/Utilities.cs
@@ -1,10 +1,23 @@
 ﻿using System;
+using System.Linq;
+using PIMS.Core.Models;
+using PIMS.Data.Repositories;
 
 
 namespace PIMS.Web.Api.Common
 {
+
     public static class Utilities
     {
+        public static Guid GetInvestorId(IGenericRepository<Investor> repositoryInvestor, string investorLogin) {
+            return repositoryInvestor.Retreive(i => i.EMailAddr.Trim() == investorLogin.Trim()).First().InvestorId;
+        }
+
+        public static string GetBaseUrl(string sourceUrl)
+        {
+            var baseIdx = sourceUrl.LastIndexOf("api/", System.StringComparison.Ordinal);
+            return sourceUrl.Substring(0, baseIdx + 4);
+        }
         
 
     }
