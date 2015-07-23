d4d98955a325f353df50aa13c9c55fba5ef1475a
Richard P. Asch <rpasch@rpclassics.net>
2015-07-23 14:46:50 -0700

    Removed unnecessary attr.

--- PIMS.Core/Models/Position.cs
+++ PIMS.Core/Models/Position.cs
@@ -16,19 +16,11 @@ namespace PIMS.Core.Models
         // NH - composite FK ref.
         public virtual Guid PositionAssetId { get; set; }
 
-        // NH - composite FK ref.
-        //public virtual Guid PositionInvestorId { get; set; }
-
         // NH - for PK mapping
         public virtual Guid PositionId { get; set; }
 
         // NH - 'one' side of Position/AccountType rel.
         public virtual AccountType Account { get; set; }
-
-        //TODO: Superflouous - Income is associated with Asset already.
-        // NH - 'many' side of Position/Income rel.
-        //public virtual Income PositionIncome { get; set; }
-        //public virtual IList<Income> PositionIncomes { get; set; } // added 7-10-15
 
         // NH - FK ref. 
         public virtual Guid AcctTypeId { get; set; }
@@ -44,7 +36,6 @@ namespace PIMS.Core.Models
         [Range(1,10000)]
         public virtual int Quantity { get; set; }
         
-        // Useful for most recent unit price info in AssetSummaryVm.
         [Required]
         public virtual DateTime LastUpdate { get; set; }
 
