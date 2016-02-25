42aea1c065cb0ce37a47efa8cd7aebc4e3b473bc
Richard P. Asch <rpasch@rpclassics.net>
2016-02-25 10:53:48 -0800

    Added RebuildTickerDesc() to handle commas in desc.

--- PIMS.Data/YahooParser.cs
+++ PIMS.Data/YahooParser.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using PIMS.Core.Models;
 
@@ -12,38 +13,49 @@ namespace PIMS.Data
             Profile profile = null;
             var rows = csvData.Replace("\r", "").Split('\n');
 
-            // Yahoo params: &f=n s b2 dyreqr1
-            foreach (var newProfile in 
-                    from row in rows 
-                    where !string.IsNullOrEmpty(row) 
-                    select row.Split(',') into cols 
+            // Trap for ticker description containing a comma.
+            if (rows[0].Split(',').Length >= 10)
+                rows[0] = RebuildTickerDesc(rows[0].Split(',')); 
+            
+                // Yahoo params: &f=n s b2 dyreqr1
+                foreach (var newProfile in 
+                    from row in rows
+                    where !string.IsNullOrEmpty(row)
+                    select row.Split(',')
+                    into cols
                     select new Profile
-                                    {
-                                        AssetId           = Guid.NewGuid(),
-                                        TickerDescription = cols[0].Replace("\"", ""),
-                                        TickerSymbol      = cols[1].Replace("\"", ""),
-                                        Price             = cols[2] == "N/A" ? 0 : Convert.ToDecimal(cols[2]),
-                                        DividendRate      = cols[3] == "N/A" ? 0 : Convert.ToDecimal(cols[3]),
-                                        DividendYield     = cols[4] == "N/A" ? 0 : Convert.ToDecimal(cols[4]),
-                                        PE_Ratio          = cols[5] == "N/A" ? 0 : Convert.ToDecimal(cols[5]),
-                                        EarningsPerShare  = cols[6] == "N/A" ? 0 : Convert.ToDecimal(cols[6]),
-                                        ExDividendDate    = cols[7] == "N/A" ? new DateTime(1900, 1, 1) : DateTime.Parse(BuildDateString(cols[7])),
-                                        DividendPayDate   = cols[8] == "N/A" ? new DateTime(1900, 1, 1) : DateTime.Parse(BuildDateString(cols[8])),
-                                        LastUpdate        = DateTime.Now,
-                                        DividendFreq      = "TBD", 
-                                    }
+                           {
+                               AssetId = Guid.NewGuid(),
+                               TickerDescription = cols[0].Replace("\"", ""),
+                               TickerSymbol = cols[1].Replace("\"", ""),
+                               Price = cols[2] == "N/A" ? 0 : Convert.ToDecimal(cols[2]),
+                               DividendRate = cols[3] == "N/A" ? 0 : Convert.ToDecimal(cols[3]),
+                               DividendYield = cols[4] == "N/A" ? 0 : Convert.ToDecimal(cols[4]),
+                               PE_Ratio = cols[5] == "N/A" ? 0 : Convert.ToDecimal(cols[5]),
+                               EarningsPerShare = cols[6] == "N/A" ? 0 : Convert.ToDecimal(cols[6]),
+                               ExDividendDate =
+                                   cols[7] == "N/A"
+                                       ? new DateTime(1900, 1, 1)
+                                       : DateTime.Parse(BuildDateString(cols[7])),
+                               DividendPayDate =
+                                   cols[8] == "N/A"
+                                       ? new DateTime(1900, 1, 1)
+                                       : DateTime.Parse(BuildDateString(cols[8])),
+                               LastUpdate = DateTime.Now,
+                               DividendFreq = "TBD",
+                           }
                     )
-              
-      
-            {
-                profile = newProfile;
+
+
+                {
+                    profile = newProfile;
+                }
+
+                return profile;
             }
 
-            return profile;
-        }
-
         // A hack for unsuccessful use of Replace(), etc. in supressing escape chars in CSV data.
-        private static string BuildDateString(string sourceDate)
+        private static string BuildDateString (string sourceDate)
         {
             var targetDate = string.Empty;
             for (var i = 0; i < sourceDate.Length; i++)
@@ -56,8 +68,25 @@ namespace PIMS.Data
         }
 
 
+        private static string RebuildTickerDesc (IList<string> index0DataRecvd)
+        {
+            var newTickerDesc = index0DataRecvd[0] + " " + index0DataRecvd[1];
+            var newString = newTickerDesc + ","
+                                          + index0DataRecvd[2] + ","
+                                          + index0DataRecvd[3] + ","
+                                          + index0DataRecvd[4] + ","
+                                          + index0DataRecvd[5] + ","
+                                          + index0DataRecvd[6] + ","
+                                          + index0DataRecvd[7] + ","
+                                          + index0DataRecvd[8] + ","
+                                          + index0DataRecvd[9];
+            return newString;
+        }
+
+
 
     }
 
 
 }
+
