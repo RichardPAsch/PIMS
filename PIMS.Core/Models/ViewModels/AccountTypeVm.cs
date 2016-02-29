e059ab626231bf6a09438f17ce4b2324b60e31a4
Richard P. Asch <rpasch@rpclassics.net>
2016-02-29 12:25:54 -0800

    Commented PositionRefId; not needed.

--- PIMS.Core/Models/ViewModels/AccountTypeVm.cs
+++ PIMS.Core/Models/ViewModels/AccountTypeVm.cs
@@ -7,11 +7,15 @@ namespace PIMS.Core.Models.ViewModels
     public class AccountTypeVm
     {
         public Guid KeyId { get; set; }
-        public Guid PositionRefId { get; set; }
+
+        // 2.29.16 - This vm should only serve as a lookup container for AccountType, need to re-evaluate need for PositionRefId, as Position table 
+        //           now rferences assets & positions?
+        //public Guid PositionRefId { get; set; }
 
         [Required]
         public string AccountTypeDesc { get; set; }
 
+        // 2.29.16 - Keep for now, as used by MapVmToAccountType(). 
         public string Url { get; set; }
     }
 }
