e0acd6f00a48051c71d43df02b77eeefe017014a
Richard P. Asch <rpasch@rpclassics.net>
2016-06-21 15:09:16 -0700

    Added GetInvestor() & GetDaysInMonth().

--- PIMS.Web.API/Common/Utilities.cs
+++ PIMS.Web.API/Common/Utilities.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using PIMS.Core.Models;
+using PIMS.Core.Security;
 using PIMS.Data.Repositories;
 
 
@@ -9,6 +10,9 @@ namespace PIMS.Web.Api.Common
 
     public static class Utilities
     {
+        //private static IPimsIdentityService _identityService;
+
+
         public static Guid GetInvestorId(IGenericRepository<Investor> repositoryInvestor, string investorLogin) {
             return repositoryInvestor.Retreive(i => i.EMailAddr.Trim() == investorLogin.Trim()).First().InvestorId;
         }
@@ -18,8 +22,16 @@ namespace PIMS.Web.Api.Common
             var baseIdx = sourceUrl.LastIndexOf("api/", System.StringComparison.Ordinal);
             return sourceUrl.Substring(0, baseIdx + 4);
         }
-        
 
+        public static string GetInvestor(IPimsIdentityService identityService)
+        {
+            return identityService.CurrentUser ?? "rpasch2@rpclassics.net";
+        }
+
+        public static int GetDaysInMonth(int year, int month)
+        {
+            return DateTime.DaysInMonth(year, month);
+        }
     }
 
         
