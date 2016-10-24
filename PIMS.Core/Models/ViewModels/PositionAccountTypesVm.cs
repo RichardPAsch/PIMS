fdf1c14e6dea0a00382cff655ca3ab9ecb4f226f
Richard P. Asch <rpasch@rpclassics.net>
2016-10-24 11:53:44 -0700

    New addition

---
+++ PIMS.Core/Models/ViewModels/PositionAccountTypesVm.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace PIMS.Core.Models.ViewModels
+{
+    public class PositionAccountTypesVm
+    {
+        [Required]
+        public string PositionAccountType { get; set; }
+
+        
+    }
+}
