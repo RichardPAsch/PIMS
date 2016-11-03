f09bd82615f9c85de459061c58b0ae21dc4ca3f7
Richard P. Asch <rpasch@rpclassics.net>
2016-11-03 15:32:57 -0700

    Updated:  /ViewModels

---
+++ PIMS.Core/Models/ViewModels/PositionsVm.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace PIMS.Core.Models.ViewModels
+{
+    public class PositionsVm
+    {
+        [Required]
+        public string PositionAccountType { get; set; }
+
+        public string PositionTickerSymbol { get; set; }
+
+    }
+
+
+
+}
