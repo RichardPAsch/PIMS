073caeeb68c3e10a70a9a7e275baf63d6bcad658
Richard P. Asch <rpasch@rpclassics.net>
2017-01-29 20:31:30 -0800

    Modified/Added view models.

---
+++ PIMS.Core/Models/ViewModels/PositionsByAssetVm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+// Usage: HttpGet via GetPositionsByAssetForEdits(string tickerSymbol)
+namespace PIMS.Core.Models.ViewModels
+{
+    public class PositionsByAssetVm
+    {
+        [Required]
+        public string PreEditPositionAccount { get; set; }
+
+        [Required]
+        public int Qty { get; set; }
+
+        [Required]
+        public decimal UnitCost { get; set; }
+
+        [Required]
+        public DateTime? DateOfPurchase { get; set; }
+
+        [Required]
+        public DateTime? LastUpdate { get; set; }
+
+        [Required]
+        public string ReferencedTickerSymbol { get; set; }
+
+        [Required]
+        public DateTime? DatePositionAdded { get; set; }
+
+
+        public Guid PositionId { get; set; }
+
+    }
+}
