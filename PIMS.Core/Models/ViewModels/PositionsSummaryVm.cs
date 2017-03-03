f80387c52b7efaf015cd1353bbb1348f2a1fd713
Richard P. Asch <rpasch@rpclassics.net>
2017-03-02 17:11:41 -0800

    Initial creation.

---
+++ PIMS.Core/Models/ViewModels/PositionsSummaryVm.cs
@@ -0,0 +1,20 @@
+﻿
+namespace PIMS.Core.Models.ViewModels
+{
+    public class PositionsSummaryVm
+    {
+        public string PositionSummaryTickerSymbol { get; set; }
+
+        public string PositionSummaryAccountType { get; set; }
+
+        public int PositionSummaryQty { get; set; }
+
+        public decimal PositionSummaryGainLoss { get; set; }
+
+        public decimal PositionSummaryValuation { get; set; }
+        
+    }
+
+
+
+}
