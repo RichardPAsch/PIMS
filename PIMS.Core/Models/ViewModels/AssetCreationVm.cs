cfa6b369dbe41a7fa9d2ab3ba554ae036e75bd4e
Richard P. Asch <rpasch@rpclassics.net>
2017-03-31 16:21:57 -0700

    Updated

--- PIMS.Core/Models/ViewModels/AssetCreationVm.cs
+++ PIMS.Core/Models/ViewModels/AssetCreationVm.cs
@@ -17,18 +17,22 @@ namespace PIMS.Core.Models.ViewModels
 
         [Required]
         public string AssetClassification { get; set; }                 // Map -> AssetClass; recvd from client
+
         public string AssetClassificationId { get; set; }               // Map -> Asset; persist to Db
         
         public ProfileVm ProfileToCreate { get; set; }
         
         
         public PositionVm PositionToCreate { get; set; }                // includes AccountType
+
         [Required]                                                      // added 5.10.16 for model state validation check
         public List<PositionVm> PositionsCreated { get; set; }
+
         public string AssetIdentification { get; set; }                 // Map -> Position Asset ref.
  
 
         public IncomeVm IncomeToCreate { get; set; }
+
         public List<IncomeVm> RevenueCreated { get; set; }              // Optional
 
 
