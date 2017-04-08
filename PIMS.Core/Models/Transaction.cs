e40177a875aaac93fdba40ce5de3d061689edc3c
Richard P. Asch <rpasch@rpclassics.net>
2017-04-07 17:48:48 -0700

    Initial creation to support Position editing.

---
+++ PIMS.Core/Models/Transaction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace PIMS.Core.Models
+{
+    public class Transaction
+    {
+        [Required]
+        public virtual Guid TransactionId { get; set; }
+
+        [Required]
+        public virtual Guid PositionId { get; set; }
+
+        public virtual string Action { get; set; }
+
+        public virtual int Units { get; set; }
+
+        public virtual decimal UnitPrice { get; set; }
+
+        public virtual decimal Fees { get; set; }
+
+        public virtual DateTime Date { get; set; }
+
+        public virtual decimal UnitCost { get; set; }
+
+        public virtual decimal CostBasis { get; set; }
+
+        public virtual decimal Valuation { get; set; }
+
+
+        // NH - references 'one' side of Position/Transaction rel.
+        public virtual Position TransactionPosition { get; set; }
+
+
+
+    }
+}
