2abceebeca58f1fbff994b64e2d4bf1ed2a04301
Richard P. Asch <rpasch@rpclassics.net>
2017-04-07 18:02:00 -0700

    Added  x.For<IGenericRepository<Transaction>>().Use<TransactionRepository>(); to support Position editing.

--- PIMS.Web.API/DependencyResolution/IoC.cs
+++ PIMS.Web.API/DependencyResolution/IoC.cs
@@ -75,6 +75,7 @@ namespace PIMS.Web.Api.DependencyResolution {
                             x.For<IGenericRepository<Investor>>().Use<InvestorRepository>();
                             x.For<IGenericRepository<Profile>>().Use<ProfileRepository>();
                             x.For<IGenericRepository<Asset>>().Use<AssetRepository>();
+                            x.For<IGenericRepository<Transaction>>().Use<TransactionRepository>();
                             //x.For<IGenericRepository<Profile>>().Use<InMemoryProfileRepository>(); [10/24/14: not needed due to unit test configurations]
                             //x.For<IGenericRepository<Position>>().Use<PositionRepository>();
                             //x.For<IGenericRepository<Income>>().Use<IncomeRepository>();
