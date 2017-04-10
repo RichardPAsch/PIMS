8f899a006ea23085aead4ea15bc1b17400fff524
Richard P. Asch <rpasch@rpclassics.net>
2017-04-10 15:49:05 -0700

    Initial creation.

---
+++ PIMS.Core/Models/ViewModels/TransactionVm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace PIMS.Core.Models.ViewModels
+{
+    public class TransactionVm
+    {
+        public Guid PositionId { get; set; }
+        public Guid TransactionId { get; set; }
+        public int Units { get; set; }
+        public decimal MktPrice { get; set; }
+        public decimal Fees { get; set; }
+        public decimal UnitCost { get; set; }
+        public  decimal CostBasis { get; set; }
+        public decimal Valuation { get; set; }
+    }
+}
