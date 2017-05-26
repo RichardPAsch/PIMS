b6ece3bcbec766b018e224f89bcce7e88f2d9139
Richard P. Asch <rpasch@rpclassics.net>
2017-05-26 11:45:51 -0700

    Initial creation for handling trx processing.

---
+++ PIMS.Data/Repositories/ITransactionEditsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using PIMS.Core.Models;
+
+
+namespace PIMS.Data.Repositories
+
+{
+    public interface ITransactionEditsRepository<T> where T: Transaction
+    {
+        
+        bool UpdateTransactions(IEnumerable<T> transactions);
+
+
+
+    }
+
+}
