023eb095dc4a62d0f876aee7c6e3ee1aad43d0f5
Richard P. Asch <rpasch@rpclassics.net>
2017-07-12 16:25:15 -0700

    Added 'ReferencedTransaction'.

--- PIMS.Core/Models/ViewModels/PositionVm.cs
+++ PIMS.Core/Models/ViewModels/PositionVm.cs
@@ -30,6 +30,8 @@ namespace PIMS.Core.Models.ViewModels
         // TODO: 5.13.16 - Shouldn't this be of type atring? Or is it really needed?
         public AccountTypeVm ReferencedAccount { get; set; }
 
+        public TransactionVm ReferencedTransaction { get; set; }
+
         public Guid CreatedPositionId { get; set; }
 
         public Guid ReferencedAssetId { get; set; }
@@ -40,6 +42,7 @@ namespace PIMS.Core.Models.ViewModels
 
         public string Status { get; set; }
 
+        // TODO: 7.12.17 - possibly obsolete? use Transaction.Fees instead ?
         public decimal TransactionFees { get; set; }
     }
     
