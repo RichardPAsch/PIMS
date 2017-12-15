9e4026a924b3fb156c98c1e59e51de2eacd449d5
Richard P. Asch <rpasch@rpclassics.net>
2017-12-14 18:38:28 -0800

    Initial creation for processing revenue XLS.

---
+++ PIMS.Core/Models/ViewModels/AssetIncomeVm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace PIMS.Core.Models.ViewModels
+{
+    /*
+        Temporary data receptacle for storing requisite Asset & Position
+        data necessary for persisting Income; this reference data is a 
+        necessary part of the XLS revenue parsing when recording income.
+    */
+    public class AssetIncomeVm
+    {
+        public string RevenueTickerSymbol { get; set; }
+
+        public Guid RevenueAssetId { get; set; }
+
+        public Guid RevenuePositionId { get; set; }
+
+        public string RevenueAccount { get; set; }
+    }
+
+}
