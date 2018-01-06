326c6c1c491301ebfc54263863051f25663772a4
Richard P. Asch <rpasch@rpclassics.net>
2018-01-05 17:26:46 -0800

    Initial creation.

---
+++ PIMS.Core/Models/ViewModels/HttpResponseVm.cs
@@ -0,0 +1,10 @@
+﻿
+namespace PIMS.Core.Models.ViewModels
+{
+    public class HttpResponseVm
+    {
+        // Generic response model for WebApi responses. Allows for
+        // future extensions.
+        public string ResponseMsg { get; set; }
+    }
+}
