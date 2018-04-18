bce6f567626504a7da4b2d1e593bb6cc77c90fee
Richard P. Asch <rpasch@rpclassics.net>
2018-04-18 15:48:21 -0700

    Add files via upload
    Deleted changes?

---
+++ AssetSummaryVm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace PIMS.Core.Models.ViewModels
+{
+    public class AssetSummaryVm
+    {
+        // View model used in "Asset/Retreive-Quick Edits" Summary menu option which 
+        // allows for selected editing while reviewing varied portfolio asset attributes.
+        
+
+        // Profile table.
+        public string TickerSymbol { get; set; }            //Read-Only
+        public string TickerSymbolDescription { get; set; }
+        public string DividendFrequency { get; set; }
+
+        // Asset table.
+        public string AssetClassification { get; set; }
+
+        //Position table.
+        public string AccountTypePostEdit { get; set; }
+        public string AccountTypePreEdit { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        // Income table.
+        public decimal IncomeRecvd { get; set; }
+        public DateTime DateRecvd { get; set; }
+
+        // TODO: needed?
+        public string CurrentInvestor { get; set; }
+    }
+
+
+    public class AssetDetailVm
+    {
+        public string TickerSymbol { get; set; }
+        public string AssetInvestor { get; set; }
+        public string AssetClassification { get; set; }
+        public string ProfileUrl { get; set; }
+        public string PositionsUrl { get; set; }
+        public string RevenueUrl { get; set; }
+
+    }
+
+    public class AssetSummaryQueryVm
+    {
+        public string TickerSymbol { get; set; }
+        public string TickerDescription { get; set; }
+        public string AssetClassification { get; set; }
+    }
+
+}
