1b7360a07735e226331420f2efa0931731dfcb7a
Richard P. Asch <rpasch@rpclassics.net>
2018-05-09 12:25:37 -0700

    Add files via upload
    Created for asset type editing.

---
+++ AssetTypeEditsRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using NHibernate;
+using PIMS.Core.Models;
+
+
+namespace PIMS.Data.Repositories
+{
+    public class AssetTypeEditsRepository : IAssetTypeEditsRepository<Asset>
+    {
+        private readonly ISession _nhSession;
+
+        public AssetTypeEditsRepository(ISessionFactory sessFactory)
+        {
+            if (sessFactory == null)
+                throw new ArgumentNullException("sessFactory");
+
+            _nhSession = sessFactory.OpenSession();
+            _nhSession.FlushMode = FlushMode.Auto;
+        }
+
+
+
+        public bool UpdateAssetTypes(IEnumerable<Asset> assetTypes)
+        {
+            var trx =  _nhSession.BeginTransaction();
+            foreach (var type in assetTypes) {
+                try {
+                    _nhSession.Merge(type);
+                    trx.Commit(); 
+                }
+                catch (Exception ex) {
+                    var res = ex.Message;
+                    return false;
+                }
+
+                if (!trx.IsActive)
+                    trx =  _nhSession.BeginTransaction();
+            }
+
+            trx.Dispose();
+            return true;
+        }
+    }
+}
---
+++ IAssetTypeEditsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using PIMS.Core.Models;
+
+
+
+namespace PIMS.Data.Repositories
+{
+    public interface IAssetTypeEditsRepository<T> where T: Asset
+    {
+        
+        bool UpdateAssetTypes(IEnumerable<T> assetTypes);
+
+    }
+
+}
